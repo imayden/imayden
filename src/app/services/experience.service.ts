f321ebf2e03e420ab0155d3a128dcf4928bfe3ca
imayden <ayden.yiming.deng@gmail.com>
2024-08-27 18:23:47 -0400

    update

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -14,22 +14,22 @@ export interface Experience {
 export class ExperienceService {
   private experiencesSubject: BehaviorSubject<Experience[]> = new BehaviorSubject<Experience[]>([
     {
-      employer: 'string',
-      title: 'string',
-      duration: 'string',
-      description: 'string'
+      employer: 'Xiaomi',
+      title: 'Product Engineer, MIUI & HyperOS',
+      duration: 'Sep, 2021 - Jul, 2022',
+      description: "Built websites and system UI features for MIUI / HyperOS, Xiaomi's custom AOSP UI for its smartphones & tablets. Led the design and development of MIUI's showcase site and owned the product HyperOS Dark Theme UI."
     },
     {
-      employer: 'string',
-      title: 'string',
-      duration: 'string',
-      description: 'string'
+      employer: 'Baidu',
+      title: 'Product Engineer, DU Shop Ecomm',
+      duration: 'May, 2021 - Aug, 2021',
+      description: "Owned a product that was part of DU Shop's e-comm suites, and led the building of the app features to enhance the buyers' shopping experience in the live room."
     },
     {
-      employer: 'string',
-      title: 'string',
-      duration: 'string',
-      description: 'string'
+      employer: 'Kwai',
+      title: 'Product Engineer, Kwai Shop',
+      duration: 'May, 2020 - Apr, 2021',
+      description: 'Crafted features for a distribution platform that was part of Kwai Shop, and led design and development on the Kwai Shop Creator Marketplace for enabling efficient cooperation between merchants & creates on Kwai.'
     },
   ]);
 
