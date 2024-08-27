26d067fba9b1d859910fafe46b9f19fa6a95cd32
imayden <ayden.yiming.deng@gmail.com>
2024-08-27 19:03:43 -0400

    update

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { ExperienceService, Experience } from '../../services/experience.service';
 import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Observable } from 'rxjs';
@@ -12,13 +13,31 @@ import { Observable } from 'rxjs';
 })
 export class AboutComponent implements OnInit {
   experiences$!: Observable<Experience[]>;
+  currentTime: string = '';
 
-  constructor(private experienceService: ExperienceService){
+  constructor(private experienceService: ExperienceService,  @Inject(PLATFORM_ID) private platformId: Object){
     this.experiences$ = this.experienceService.experiences$;
   }
 
   ngOnInit(): void {
-      
+      this.myTimeZone();
+      if (isPlatformBrowser(this.platformId)) {
+        setInterval(() => this.myTimeZone(), 1000);
+
+      }
+
+  }
+
+  myTimeZone():void{
+    const date = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: 'America/New_York',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    };
+    this.currentTime = new Intl.DateTimeFormat('en-US', options).format(date);
   }
 
 }
